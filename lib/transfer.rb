dd33dd6b76e9ae366a37cf2a084553920f12c3af
Alan Tang <giabao_tang@yahoo.com>
2020-10-11 22:13:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else 
-      status = "rejected"
+      self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
